ba9da2f10647c9289422f1fe9960f6ea11553e7a
jonburleson <24725561+jonburleson@users.noreply.github.com>
2020-02-25 00:35:55 -0600

    Edited settings and created a dark mode

--- app/src/main/java/com/jonburleson/utilitybelt/MainActivity.java
+++ app/src/main/java/com/jonburleson/utilitybelt/MainActivity.java
@@ -1,17 +1,22 @@
 package com.jonburleson.utilitybelt;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import com.jonburleson.utilitybelt.R;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
+import androidx.preference.PreferenceManager;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPref = null;
+    SharedPreferences.OnSharedPreferenceChangeListener spListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +25,69 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        updateNightMode();
+        spListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+                    @Override
+                    public void onSharedPreferenceChanged(SharedPreferences sharedPref,
+                                                          String key) {
+                        switch (key) {
+                            case "darkMode":
+                                //Toast.makeText(MainActivity.this, "Restart needed to take effect", Toast.LENGTH_SHORT).show();
+                                updateNightMode();
+                                break;
+                            default :
+                                break;
+                        }
+                    }
+                };
+        sharedPref.registerOnSharedPreferenceChangeListener(spListener);
     }
+
+    public void updateNightMode() {
+        boolean darkValue = sharedPref.getBoolean(SettingsActivity.KEY_PREF_DARK_MODE, false);
+        if (darkValue) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
+
+    //@Override
+    //public void onStart() {
+    //    super.onStart();
+    //}
+
+    //@Override
+    //public void onResume() {
+    //    super.onResume();
+    //    sharedPref.registerOnSharedPreferenceChangeListener(spListener);
+    //}
+
+    //@Override
+    //public void onPause() {
+    //    super.onPause();
+    //    sharedPref.unregisterOnSharedPreferenceChangeListener(spListener);
+    //}
+
+    //@Override
+    //public void onStop() {
+    //    super.onStop();
+    //}
+
+    //@Override
+    //public void onRestart() {
+    //    super.onRestart();
+    //}
+
+    //@Override
+    //public void onDestroy() {
+    //    super.onDestroy();
+    //    sharedPref.unregisterOnSharedPreferenceChangeListener(spListener);
+    //}
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/jonburleson/utilitybelt/SettingsActivity.java
+++ app/src/main/java/com/jonburleson/utilitybelt/SettingsActivity.java
@@ -1,10 +1,12 @@
 package com.jonburleson.utilitybelt;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    public static final String
+            KEY_PREF_DARK_MODE = "darkMode";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +15,4 @@ public class SettingsActivity extends AppCompatActivity {
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
     }
-
 }
